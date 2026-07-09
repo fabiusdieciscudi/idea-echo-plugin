ae5f83fe1cd1fb6bc1f81914b0080a6f106afa58
Fabius Dieciscudi <fabius.dieciscudi@protonmail.com>
2026-07-09 15:43:35 +0200

    WIP: URL

--- src/main/kotlin/name/fabius10scudi/ideaecho/EchoConfig.kt
+++ src/main/kotlin/name/fabius10scudi/ideaecho/EchoConfig.kt
@@ -7,7 +7,8 @@ object EchoConfig {
     const val VOWELS = "aeiouàèéìíòóùú"
 
     /** REST endpoint of the synonyms/antonyms dictionary. %s = URL-encoded word. */
-    const val THESAURUS_URL_TEMPLATE = "https://sapere.virgilio.it/parole/sinonimi-e-contrari/%s"
+//    const val THESAURUS_URL_TEMPLATE = "https://sapere.virgilio.it/parole/sinonimi-e-contrari/%s"
+    const val THESAURUS_URL_TEMPLATE = "https://synonyms.reverso.net/sinonimi/it/%s"
 
     /** LaTeX commands whose argument (and the command itself) must be fully ignored. */
     val IGNORED_COMMANDS: Set<String> = setOf("comment", "note", "scene", "beat", "ellipsis", "temporaljump") // command names, no backslash
